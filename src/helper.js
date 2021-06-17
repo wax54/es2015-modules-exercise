39ab659a7433fcea4d8d6b6adcec09a7b1b8be98
sam <wax654@gmail.com>
2021-06-16 20:13:01 -0700

    debugged issue

--- src/helper.js
+++ src/helper.js
@@ -1,18 +1,23 @@
 const randBetween = (min, max) => {
     //get the diff
-    const diff = max - min;
+    const diff = (max - min) + 1;
     //get a random num between that diff
-    const randNum = Math.floor((Math.random * diff) + 1);
-    //add the min back to get a rand num between (exclusive) min and max
+    const randNum = Math.floor(Math.random() * diff);
+    //add the min back to get a rand num between (inclusive) min and max
     return randNum + min;
 }
-const randIndex = array => randBetween(-1, array.length);
+const randIndex = array => randBetween(0, array.length - 1);
 
-const choice = items => items[randIndex];
+const choice = items => {
+    return items[randIndex(items)];
+}
 
 const remove = (array, item) => {
+    console.log(item);
     const idx = array.findIndex(element => element === item);
-    delete array[idx];
+    console.log(idx);
+    array.splice(idx, 1);
+    console.log(array)
     return item;
 }
 
